9f9ea2b8869b11a0f742ed2ac240135e196e3504
Nelly <neo71a@rambler.ru>
2022-12-09 11:20:57 +0300

    1

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 15
+
 Console.WriteLine("Hello, World!");
+Console.Clear();
+
+Console.Write("Узнать выходной день! ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1 && n <=5)
+{
+    Console.Write("Это не выходной!");
+}
+else if (n >=6 && n <=7)
+{
+    Console.Write("Ура, выходной день!");
+}
+else Console.Write("Нет такого дня недели.");
