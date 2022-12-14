92c7ca07e582e1f90a8ce1c36d56b5b242fca7c1
Nelly <neo71a@rambler.ru>
2022-12-14 19:00:16 +0300

    Ex

---
+++ Ex/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("max="+x);
+    Console.WriteLine("min="+y);
+}
+else
+{
+    Console.WriteLine("max="+x);
+    Console.WriteLine("min="+y);
+}
