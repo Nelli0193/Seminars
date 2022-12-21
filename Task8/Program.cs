cc143f8e5c471f53dfb02760e37ecdd0113b26c3
Nelly <neo71a@rambler.ru>
2022-12-21 17:26:38 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int OneNumber = 2;
+
+if(number > 1)
+{
+    while(OneNumber <= number)
+    {
+        Console.Write(OneNumber + " ");
+        OneNumber = OneNumber + 2;
+    }
+}
